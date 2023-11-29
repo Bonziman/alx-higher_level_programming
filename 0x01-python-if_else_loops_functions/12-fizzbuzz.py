1035c515ff10660f52664b49c0543f006a0e434d
Bonziman <eloirdiwiggmail.com>
2023-11-29 05:04:48 -0800

    name typo

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,12 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 15 == 0:
             print('FizzBuzz', end=' ')
+            continue
         if i % 3 == 0:
             print('Fizz', end=' ')
+            continue
         elif i % 5 == 0:
             print('Buzz', end=' ')
+            continue
         else:
             print(i, end=' ')
