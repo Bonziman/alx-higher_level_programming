f9fa53ae15bd38e7a69517593797722b18051e48
Bonziman <eloirdiwiggmail.com>
2023-12-03 06:30:06 -0800

    print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 def print_list_integer(my_list=[]):
     list = my_list
     for i in range(0, len(list)):
-        print('{}'.format(list[i]))
+        str = '{}'
+        print(str.format(list[i]))
