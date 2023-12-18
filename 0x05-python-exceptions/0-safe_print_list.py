2332af24af35f01536e5afb483df91cacd4be6b4
Bonziman <eloirdiwi@gmail.com>
2023-12-18 22:03:22 +0100

    fixed pycodestyle in task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             if count < x:
                 print("{:d}".format(element), end='')
                 count += 1
-        except: TypeError as e:
+        except TypeError as e:
             print("Eror:", e)
     print()
     return count
