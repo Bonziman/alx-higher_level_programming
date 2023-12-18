03acbf49af9503d2f19ff91f1ac74931960a55e9
Bonziman <eloirdiwi@gmail.com>
2023-12-18 21:35:59 +0100

    used try: and exception:

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for element inmy_list:
-        if count < x:
-            print("{:d}".format(element), end='')
-            count += 1
+    try:
+        for element inmy_list:
+            if count < x:
+                print("{:d}".format(element), end='')
+                count += 1
+        except: TypeError as e:
+            print("Eror:", e)
     print()
     return count
