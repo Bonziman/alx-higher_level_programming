8b5468476e1194be8e0cf552425801fbe48daf08
Bonziman <eloirdiwi@gmail.com>
2023-12-19 23:47:27 +0100

    give a size attr to the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines a square class."""
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes a square with a size."""
+        self.__size = size
