74ea8a52349cedbf9d3a81b79867062b12ef7398
Bonziman <eloirdiwi@gmail.com>
2023-12-20 00:09:09 +0100

    a pain in the ear

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""this module defies a Square class."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size=0):
+        """initialization of the square with a size.
+        Args:
+            size (int, optional): the size of the square dflt to 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
