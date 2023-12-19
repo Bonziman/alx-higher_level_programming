524cf3ddc91c7513440850610c5904ed8ca01631
Bonziman <eloirdiwi@gmail.com>
2023-12-19 23:00:10 +0100

    readme file

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Represents a square.
+
+    This class can be used to create square objects.
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+starting off with python classes AKA OOP
