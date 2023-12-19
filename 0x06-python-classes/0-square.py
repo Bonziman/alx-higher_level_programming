b78ceb1ee8e18e3c2f565563359706781e456285
Bonziman <eloirdiwi@gmail.com>
2023-12-19 23:20:29 +0100

    no need to init

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""
-This module defines a Square class.
-
-The Square class represents a square shape.
-"""
+"""This module defines a Square class. The Square class represents a square shape."""
 
 class Square:
-    """
-    Represents a square.
-
-    This class can be used to create square objects.
-    """
-
-        def __init__(self):
-            """
-            Initializes an empty square.
-            """
-            pass
+    """Initializes an empty square"""
+    pass
 
