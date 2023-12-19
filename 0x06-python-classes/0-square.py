da8b0456b95b251b8f24734584ef6d2edd6773e3
Bonziman <eloirdiwi@gmail.com>
2023-12-19 23:13:04 +0100

    forgot module cod

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+"""
+This module defines a Square class.
+
+The Square class represents a square shape.
+"""
+
 class Square:
     """
     Represents a square.
 
     This class can be used to create square objects.
     """
-    pass
+
+        def __init__(self):
+            """
+            Initializes an empty square.
+            """
+            pass
+
