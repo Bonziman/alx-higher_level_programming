d18ae562316450c78ee25f31379ade41b2f4b993
Bonziman <eloirdiwi@gmail.com>
2023-12-20 21:48:20 +0100

    created the area function to calculate the area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,11 +7,17 @@ class Square:
     def __init__(self, size=0):
         """initialization of the square with a size.
         Args:
-            size (int, optional): the size of the square dflt to 0
+        size (int, optional): the size of the square dflt to 0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size
+        self.__size = s
+    def area(self):
+        """Calculate the area of a square
+        Returns:
+            int: the area of the square
+        """
+        return self.__size * self.__size
