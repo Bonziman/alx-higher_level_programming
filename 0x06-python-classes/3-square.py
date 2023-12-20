ae0ddf529f143845f067b5de4c9fae3ab92acaf9
Bonziman <eloirdiwi@gmail.com>
2023-12-20 21:57:56 +0100

    whitespace in that line

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,7 +15,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-        
+
     def area(self):
         """Calculate the area of a square
         Returns:
