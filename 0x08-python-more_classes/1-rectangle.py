df5fb62e5978f899973295946c7be224ce7904eb
Bonziman <eloirdiwi@gmail.com>
2024-01-04 16:10:57 +0100

    seperate properties

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,10 +21,6 @@ class Rectangle:
         """Returns the width of a rectangle."""
         return self.__width
 
-    def height(self):
-        """Returns the height of a rectangle."""
-        return self.__height
-
     @width.setter
     def width(slef, value):
         """set the width of a rectangle.
@@ -36,6 +32,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of a rectangle."""
+        return self.__height
 
     @height.setter
     def height(self, value):
