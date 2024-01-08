bd2d44fac6808e3a62e12595783d0ba77f7294d4
Bonziman <eloirdiwi@gmail.com>
2024-01-08 16:34:35 +0100

    3-say_my_name pycodestyle

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -9,7 +9,8 @@ def say_my_name(first_name, last_name=""):
         last_name(str): your last name
     """
     if not first_name:
-        raise TypeError("say_my_name() missing 1 required positional argument: 'first_name'")
+        raise TypeError("say_my_name() missing 1 required positional "
+                        "argument:'first_name'")
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     elif type(last_name) is not str:
