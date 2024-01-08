be2d83faea33d542fd220196791c5ad132f4b730
Bonziman <eloirdiwi@gmail.com>
2024-01-08 16:24:20 +0100

    added canot convert float NaN to integer test to the integer add function

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -9,7 +9,8 @@ def add_integer(a, b=98):
         b(int): second number to be added.
     Returns: result(the result of the addition of the 2 numbers)
     """
-
+    if a != a or b != b:
+        raise TypeError("cannot convert float NaN to integer")
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
