c9b8551e4fcdb373ad7a6365078f825f6ddbe31a
Bonziman <eloirdiwi@gmail.com>
2024-01-11 17:18:19 +0100

    cap A

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -11,4 +11,4 @@ class BaseGeometry():
             Raises:
                 Exception: this method is not implemented.
         """
-        raise Exception("Area() is not implemented")
+        raise Exception("area() is not implemented")
