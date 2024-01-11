ae6629ac163e80dd9c9b7327f15735c3ea9dabe0
Bonziman <eloirdiwi@gmail.com>
2024-01-11 16:29:02 +0100

    fixed pycodestyle for module 1-my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,14 @@
 
 
 class MyList(list):
+    """MyList class that inherites from the list"""
     def __init__(self, *args, **kwargs):
+        """Initialisation method"""
+
         super().__init__(*args, **kwargs)
 
     def print_sorted(self):
+        """Method to print the list sorted from smallest to biggest"""
+
         sorted_list = sorted(self, reverse=False)
         print(sorted_list)
