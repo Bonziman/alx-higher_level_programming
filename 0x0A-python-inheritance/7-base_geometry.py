b1319cd0a5b50d1743dbb2a66aedb7becb90ab46
Bonziman <eloirdiwi@gmail.com>
2024-01-11 17:33:59 +0100

    is it correct

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines an empty class"""
+
+
+class BaseGeometry():
+    """ An empty class"""
+
+    def area(self):
+        """
+        Calculate the area of the geometry.
+        Raises:
+                Exception: this method is not implemented
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(slef, name, value):
+        """
+        Check and validate value
+        Raises:
+            TypeError: name must be an integer
+            ValueError: name must be greater than 0
+        """
+        if not type(value) is int:
+            try:
+                raise TypeError("must be an integer")
+            except TypeError as e:
+                print("{} {}".format(name, e))
+        if type(value) is int and value <= 0:
+            try:
+                raise ValueError("must be greater than 0")
+            except ValueError as e:
+                print("{} {}".format(name, e))
--- 0x0A-python-inheritance/test.py
+++ 0x0A-python-inheritance/test.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-MyList = __import__('1-my_list').MyList
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-my_list = MyList()
-my_list.append(1)
-my_list.append(4)
-my_list.append(2)
-my_list.append(3)
-my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
-print(my_list is my_list.print_sorted())
+bg = BaseGeometry()
+
+bg.integer_validator("my_int", 12)
+bg.integer_validator("width", 89)
+
+try:
+    bg.integer_validator("name", "John")
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("age", 0)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
+
+try:
+    bg.integer_validator("distance", -4)
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
