221d73b75d7315760f53cadc124f704fa011b124
Bonziman <eloirdiwi@gmail.com>
2024-01-11 17:13:39 +0100

    this method is not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines an empty class"""
+
+
+class BaseGeometry():
+    """ An empty class"""
+    def ares(self):
+        """
+            Calculate the area of the geometry.
+            Raises:
+                Exception: this method is not implemented.
+        """
+        raise Exception("Area() is not implemented")
