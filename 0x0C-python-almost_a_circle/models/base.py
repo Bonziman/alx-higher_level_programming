a88b3ba65dcf41c8b5fd7bd965cfc7220bdbc533
Bonziman <eloirdiwi@gmail.com>
2024-01-15 23:56:34 +0100

    requirements fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,11 +3,17 @@
 
 
 class Base:
-    """ Base class """
+    """ Base class
+    which is a base for other classes
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Init function for the class
+        Args:
+            id(int): the id of the class objects
+        """
         if id is not None:
             self.id = id
         else:
