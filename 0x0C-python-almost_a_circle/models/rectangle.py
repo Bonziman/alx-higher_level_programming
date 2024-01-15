976ee3db846f6687ade38cebd5e733198d32f121
Bonziman <eloirdiwi@gmail.com>
2024-01-16 00:10:30 +0100

    more pycodestyle errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,14 +2,17 @@
 """ a rectangle class model"""
 from models.base import Base
 
+
 class Rectangle(Base):
+    """ A rectangle class that inherites from Base class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-    
+
     @property
     def width(self):
         """retrieve width of a rectangle"""
