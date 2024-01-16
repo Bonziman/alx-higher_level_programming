bc7ddbef0590299f6601a868b90f6b208b01d1a5
Bonziman <eloirdiwi@gmail.com>
2024-01-16 02:06:22 +0100

    created a Square model and class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Model that defines a square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ A Square class that inherits from Rectangle class directly
+    and from Base class indirectly
+    """
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        initialize a square instance
+        """
+        super().__init__(id, x, y, size, size)
+
+    def __str__(self):
+        """overide the default str representation
+        """
+        return ("[Square] ({}) {}/{} - {}"
+                .format(self.id, self.x, self.y, self.width))
