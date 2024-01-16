fcf14adaea9a5cc592a9ddf8be8949ee4ecc2e3b
Bonziman <eloirdiwi@gmail.com>
2024-01-16 17:13:33 +0100

    square now has a size setter and getter with same validation as the rectangle(width)

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,3 +18,25 @@ class Square(Rectangle):
         """
         return ("[Square] ({}) {}/{} - {}"
                 .format(self.id, self.x, self.y, self.width))
+
+    @property
+    def size(self):
+        """Retrieve the size(width or height) of a square instance
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Assign size ro a square instance
+
+        Args:
+            value: the value to be assigned to size
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__size = value
+            self.width = value
+            self.height = value
--- 0x0C-python-almost_a_circle/test.py
+++ 0x0C-python-almost_a_circle/test.py
@@ -2,6 +2,9 @@
 from models.square import Square
 
 s = Square(5)
-print(s.id)
+print(s)
+print(s.size)
+s.size = 10
+print(s)
 print(s.width)
 print(s.height)
