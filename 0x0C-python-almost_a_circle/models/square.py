a028e8455dbcb2aa05edd4dbcc4ae8492138fad3
Bonziman <eloirdiwi@gmail.com>
2024-01-16 17:00:25 +0100

    had the wrong attribute order passed to the syper init method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,7 +11,7 @@ class Square(Rectangle):
         """
         initialize a square instance
         """
-        super().__init__(size, size,x, y, id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """overide the default str representation
