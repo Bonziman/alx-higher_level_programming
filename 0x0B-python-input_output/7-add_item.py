2521c027e77918ace5371c2935290a8888c89ceb
Bonziman <eloirdiwi@gmail.com>
2024-01-22 16:03:22 +0100

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,9 +8,9 @@ if __name__ == "__main__":
     laod_from_json_file = \
             __import__('6-load_from_json_file').load_from_json_file
 
-        try:
-            items = load_from_json_file("add_items.json")
-        except FileNotFoundError:
-            items = []
-        items.extend(sys.argv[1:])
-        save_to_json_file(items, "add_items.json")
+    try:
+        items = load_from_json_file("add_items.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_items.json")
