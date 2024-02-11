72239aabe0fa007ef30abfcc1bbfaa2823b09781
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-02-11 21:42:27 +0100

    task 0 pycode

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = matrix.copy()
 
+def square_matrix_simple(matrix=[]):
+    """squares elemenst of a matrix
+    Args:
+        matrix(list of lists): the target matrix
+    """
+    new_matrix = matrix.copy()
     for i in range(len(matrix)):
         new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
-
     return (new_matrix)
