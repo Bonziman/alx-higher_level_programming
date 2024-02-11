dba5deeb549db3bb0505749626933332262a0c64
Aymane Eloirdiwi <103787921+Bonziman@users.noreply.github.com>
2024-02-11 21:46:30 +0100

    task 0 newline at the end

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,7 @@
 def square_matrix_simple(matrix=[]):
     """squares elemenst of a matrix
     Args:
-        matrix(list of lists): the target matrix
+        matrix(list of lists):the target matrix
     """
     new_matrix = matrix.copy()
     for i in range(len(matrix)):
