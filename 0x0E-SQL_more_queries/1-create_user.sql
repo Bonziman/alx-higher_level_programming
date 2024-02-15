3cea04fc1a29bbd8676e3d994557798560dd5319
Bonziman <eloirdiwi@gmail.com>
2024-02-15 01:32:36 +0100

    create a user and give him all privileges over mysql server

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 --script to create a user and grant it all privileges
-CRATE USER 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd' IF NOT EXISTS;
+CREATE USER IF NOT EXISTS 'user_0d_1' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1' WITH GRANT OPTION;
