b6a654496aae1c821e344eff43d9a7d66b649a8a
Bonziman <eloirdiwi@gmail.com>
2024-03-15 22:07:59 +0000

    added main function to task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     """main function"""
-    db = MySQLdb.connect(host='localhost', port=3306, user=aya.atgv[1] , passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.atgv[1] , passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
