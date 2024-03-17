c340e8219463026437ca71173b5c4d3b938582b6
Bonziman <eloirdiwi@gmail.com>
2024-03-17 20:50:59 +0000

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,8 +9,9 @@ from urllib.parse import quote_plus
 
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                            .format(sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3]))
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3])
+    engine = create_engine(db_url)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
