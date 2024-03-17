59cae1d4528eb1371546f283287f58b7295f8d39
Bonziman <eloirdiwi@gmail.com>
2024-03-17 21:46:44 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from a db"""
+import sys
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from urllib.parse import quote_plus
+
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    passw = quote_plus(sys.argv[2])
+    dbase = sys.argv[3]
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+             sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3])
+    engine = create_engine(db_url)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
+
