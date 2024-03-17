03c73c2470b049ee2528aff968eaefdfa26d6439
Bonziman <eloirdiwi@gmail.com>
2024-03-17 21:48:25 +0000

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    
+
     new_state = State(name="Louisiana")
     session.add(new_state)
     session.commit()
     print(new_state.id)
-
