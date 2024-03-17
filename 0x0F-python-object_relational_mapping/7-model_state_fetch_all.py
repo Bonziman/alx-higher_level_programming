cb115bc4eb0bb9164c9306f7c743d786cb7e0e29
Bonziman <eloirdiwi@gmail.com>
2024-03-17 20:47:15 +0000

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script that lists all State objects from a db"""
+import sys
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from urllib.parse import quote_plus
+
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                            .format(sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print(state.id, state.name, sep=", ")
