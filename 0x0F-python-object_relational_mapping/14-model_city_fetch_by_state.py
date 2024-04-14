c6ed1f97828670c9cc3d47dc61805e9caf586b1a
Bonziman <eloirdiwi@gmail.com>
2024-04-14 22:42:45 +0100

    this one was tricky

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Script that prints all City objects from the database"""
+"""List all states"""
+from sys import argv
 import sys
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import Base
-from urllib.parse import quote_plus
+from model_state import Base, State
 from model_city import City
-
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
 
 if __name__ == "__main__":
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            sys.argv[1], quote_plus(sys.argv[2]), sys.argv[3])
-    engine = create_engine(db_url)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], quote_plus(sys.argv[2]),
+            argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    cities = session.query(City).all()
-    for city in cities:
-        print(city.state.name, ":", city.id, city.name)
+    results = session.query(City, State).\
+        filter(City.state_id == State.id).all()
+    for city, state in results:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    session.commit()
+    session.close()
