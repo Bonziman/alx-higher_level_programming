41ee4ba628ea69661b4e21083a4d584dfdaec945
Bonziman <eloirdiwi@gmail.com>
2024-04-25 20:49:17 +0100

    simplified it

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #script to print the side of the body of a http header response
-url="$1"
-body_size=$(curl -I -s -w "%{size_download}" -o /dev/null "$url")
-echo "$body_size"
+curl -s "$1" | wc -c
