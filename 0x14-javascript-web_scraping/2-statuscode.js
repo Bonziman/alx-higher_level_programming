e0c6d0bbbba4c836b6a96abd2eecc3ac969bf543
Bonziman <eloirdiwi@gmail.com>
2024-05-22 22:05:53 +0100

    get status code and log it

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ request.get(url, function (error, response, body) {
   if (error) {
     console.error('error:', error);
   } else {
-    console.log('code:', response.statusCode);  
+    console.log('code:', response.statusCode);
   }
 });
