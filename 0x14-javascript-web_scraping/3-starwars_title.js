7349e6b028d05dfc6910565117e987603408714f
Bonziman <eloirdiwi@gmail.com>
2024-05-22 22:48:24 +0100

    what star wars ep are you watching?

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,12 +2,12 @@
 
 const request = require('request');
 
-const mv_id = process.argv[2];
+const mvId = process.argv[2];
 
-let url = 'https://swapi-api.alx-tools.com/api/films/' + mv_id;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + mvId;
 
-request(url, function(error, response, body) {
-  if (!error && response.statusCode == 200);
-    const info = JSON.parse(body);
-    console.log(info.title);
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200);
+  const info = JSON.parse(body);
+  console.log(info.title);
 });
