1b0dfabea5944d69d83d1ccb5752d298d6ec40ed
Bonziman <eloirdiwi@gmail.com>
2024-05-22 21:36:44 +0100

    fixed semistandard style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error('Error reading the file:', err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.error('Error reading the file:', err);
+  } else {
+    console.log(data);
+  }
 });
