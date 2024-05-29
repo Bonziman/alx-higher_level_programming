3eab7605eb9d7113c84b001cffd821cfad41a227
Bonziman <eloirdiwi@gmail.com>
2024-05-29 21:55:17 +0100

    make that element red

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,2 +1,3 @@
-const headerElement = document.querySelector('header');
-headerElement.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
