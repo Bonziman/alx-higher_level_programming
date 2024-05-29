c7aac9312885bd134f744f2e95679b2674c4cf7b
Bonziman <eloirdiwi@gmail.com>
2024-05-29 22:44:06 +0100

    green red

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+/* global $ */
+$(document).ready(function () {
+  $('#toggle_header').click(function () {
+    if ($('header').hasClass('red')) {
+      $('header').removeClass('red').addClass('green');
+    } else {
+      $('header').removeClass('green').addClass('red');
+    }
+  });
+});
